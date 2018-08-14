3bb902a1993290089d5cdc38c0ea88d1729e411c
tjjjjwxzq <aquietimmanence@gmail.com>
2018-08-14 23:55:45 +0800

    Add styling to image upload to preview and reset image on upload

--- client/src/components/ImageUpload.jsx
+++ client/src/components/ImageUpload.jsx
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
 
 import { AppContext } from './AppContext';
 import { withContext } from 'util/context';
 import { extractErrors, formatErrors } from 'util/errors';
 import { AlertType } from './Alert';
 
+import defaultImage from 'assets/image-placeholder.svg';
+
 class _ImageUpload extends Component {
   constructor(props) {
     super(props);
+
     this.fileInput = React.createRef();
+    this.state = {
+      imageSrc: defaultImage,
+    };
   }
 
   handleChange = () => {
     const file = this.fileInput.current.files[0];
+    const imageSrc = window.URL.createObjectURL(file);
+
+    this.setState({ imageSrc });
   }
 
   handleUpload = async (event) => {
@@ -35,6 +45,13 @@ class _ImageUpload extends Component {
     const response = await requestWithAlert
       .uploadFile('/api/v1/images', formData);
 
+    if (response.isSuccessful) {
+      // Doing this also resets the readonly FileList object (this.fileInput.current.files)
+      this.fileInput.current.value = '';
+      this.setState({ imageSrc: defaultImage });
+      showAlert('imageUploadSuccess', AlertType.SUCCESS, 'Your image was uploaded successfully!');
+    }
+
     if (response.hasError) {
       const errors = await extractErrors(response);
       showAlert('imageUploadFailure', AlertType.ERROR, formatErrors(errors));
@@ -42,11 +59,39 @@ class _ImageUpload extends Component {
   }
 
   render() {
+    const { classes } = this.props;
+    const file = this.fileInput.current ? this.fileInput.current.files[0] : null;
+
     return (
-      <div>
+      <div className={classes.uploadContainer} style={{ backgroundImage: `url(${this.state.imageSrc})` }}>
         <form onSubmit={this.handleUpload} >
-          <input type="file" ref={this.fileInput} onChange={this.handleChange} />
-          <input type="submit" />
+          <input
+            type="file"
+            accept="image/*"
+            ref={this.fileInput}
+            onChange={this.handleChange}
+            id="image_upload"
+            className={classes.uploadInput}
+          />
+          {file ?
+            <Button
+              type="submit"
+              color="default"
+              variant="contained"
+            >
+              Confirm Upload
+            </Button>
+            :
+            <label htmlFor="image_upload">
+              <Button
+                color="primary"
+                variant="contained"
+                component="span"
+              >
+                Upload File
+              </Button>
+            </label>
+          }
         </form>
       </div>
     );
@@ -54,8 +99,17 @@ class _ImageUpload extends Component {
 }
 
 const styles = {
-  uploadButton: {
-    borderRadius: '50%',
+  uploadContainer: {
+    width: '600px',
+    height: '400px',
+    margin: '0 auto',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundSize: 'cover',
+  },
+  uploadInput: {
+    display: 'none',
   },
 };
 
