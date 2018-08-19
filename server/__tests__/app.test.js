f89e3b4f284cbe13e2629f5ff533da8bb4483e79
AmandaOh <amandaoh@thoughtworks.com>
2018-08-19 20:27:58 +0800

    Fix linting errors

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -3,7 +3,7 @@ import request from 'supertest';
 import app from '../app';
 
 describe('Root Route', () => {
-  test('get request returns status code 200', async() => {
+  test('get request returns status code 200', async () => {
     const response = await request(app).get('/');
 
     expect(response.status).toEqual(200);
