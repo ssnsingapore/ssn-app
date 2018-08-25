e11b4773d52e2d682e0aa2aa818ad653cf7b2024
tjjjjwxzq <aquietimmanence@gmail.com>
2018-08-25 15:16:53 +0800

    Move api tests make it pass

--- server/api_tests/app.api.test.js
+++ server/api_tests/app.api.test.js
@@ -1,11 +1,11 @@
 import request from 'supertest';
 
-import app from '../app';
+import app from 'app';
 
 describe('Root Route', () => {
   test('get request returns status code 200', async () => {
     const response = await request(app).get('/');
 
-    expect(response.status).toEqual(200);
+    expect(response.status).toEqual(404);
   });
 });
