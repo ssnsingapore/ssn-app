ffae82dce9edcd3d9b5daa048a4b246da7560b25
AmandaOh <amandaoh@thoughtworks.com>
2018-08-25 22:30:15 +0800

    Replace function declaration with const assignment due to hoisting throwing ReferenceError for 'isTest'

--- server/src/config/environment.js
+++ server/src/config/environment.js
@@ -11,6 +11,31 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+const isDevelopment = process.env.NODE_ENV === 'development';
+const isTest = process.env.NODE_ENV === 'test';
+const config = {
+  PORT: process.env.PORT,
+  DATABASE_URI: process.env.DATABASE_URI,
+  AUTH_SECRET: process.env.AUTH_SECRET,
+  AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+  TOKEN_COOKIE_NAME: 'ssn_token',
+  TOKEN_COOKIE_MAXAGE: 15 * 24 * 60 * 60 * 1000,
+  JWT_EXPIRES_IN: '30 days',
+};
+
+const checkRequiredExist = (...variables) => {
+  if (isTest) {
+    return;
+  }
+
+  variables.forEach((name) => {
+    if (!process.env[name]) {
+      throw new Error(`Environment variable ${name} is missing`);
+    }
+  });
+}
+
 // Note: NODE_ENV variable is recognized by Express
 checkRequiredExist(
   'NODE_ENV',
@@ -22,27 +47,4 @@ checkRequiredExist(
   'AWS_SECRET_ACCESS_KEY',
 );
 
-export const isProduction = process.env.NODE_ENV === 'production';
-export const isDevelopment = process.env.NODE_ENV === 'development';
-export const isTest = process.env.NODE_ENV === 'test';
-export const config = {
-  PORT: process.env.PORT,
-  DATABASE_URI: process.env.DATABASE_URI,
-  AUTH_SECRET: process.env.AUTH_SECRET,
-  AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
-  TOKEN_COOKIE_NAME: 'ssn_token',
-  TOKEN_COOKIE_MAXAGE: 15 * 24 * 60 * 60 * 1000,
-  JWT_EXPIRES_IN: '30 days',
-};
-
-function checkRequiredExist(...variables) {
-  if (isTest) {
-    return;
-  }
-
-  variables.forEach((name) => {
-    if (!process.env[name]) {
-      throw new Error(`Environment variable ${name} is missing`);
-    }
-  });
-}
+export {isTest, isProduction, isDevelopment, config};
