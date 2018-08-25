334b14f1005108f7f2c4a23659a5067ab73d2701
tjjjjwxzq <aquietimmanence@gmail.com>
2018-08-25 12:56:56 +0800

    Remve uneeded public files, views and controllers

--- server/controllers/pages_controller.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-export const pagesRouter = express.Router();
-
-pagesRouter.get('/', getHome);
-function getHome(req, res) {
-  res.render('index', { title: 'Express' });
-}
