f196ac57e89dd5f67274f941684816a6caaf90a4
tjjjjwxzq <aquietimmanence@gmail.com>
2018-09-09 01:06:05 +0800

    [SSN-1][JQ] Amend homepage styling

--- client/src/components/main/HomePage.jsx
+++ client/src/components/main/HomePage.jsx
@@ -1,6 +1,6 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import { Typography, Grid } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, withTheme } from '@material-ui/core/styles';
 
 import { ProjectListing } from 'components/ProjectListing';
 
@@ -8,28 +8,30 @@ import landingImage from 'assets/bg.jpg';
 
 class _HomePage extends Component {
   render() {
-    const { classes } = this.props;
+    const { classes, theme } = this.props;
 
     return (
       <div>
         <div className={classes.landingImage}>
-          <Typography variant="headline" gutterBottom className={classes.landingHeader}>
-          SSN Project Portal
+          <Typography variant="display1" gutterBottom className={classes.landingHeader}>
+            SSN Project Portal
           </Typography>
-          <Typography variant="subheading">
+          <Typography variant="headline">
             SSN Project Portal aims to match volunteers and organisers in the Singapore Sustainability Space
           </Typography>
         </div>
 
-        <Grid container className={classes.root}>
-          <Grid item md={9} xs={12}>
-            <Typography variant="body2">
-            Find a Sustainability Project to volunteer for:
-            </Typography>
-            <ProjectListing />
-          </Grid>
-          <Grid item md={3} xs={12} className={classes.gridItem}>
-            Item 2
+        <Grid container spacing={4 * theme.spacing.unit} className={classes.root}>
+          <Typography variant="headline" gutterBottom>
+              Find a Sustainability Project to volunteer for:
+          </Typography>
+          <Grid container spacing={4 * theme.spacing.unit}>
+            <Grid item md={8} xs={12}>
+              <ProjectListing />
+            </Grid>
+            <Grid item md={3} xs={12} className={classes.gridItem}>
+              Item 2
+            </Grid>
           </Grid>
         </Grid>
       </div>
@@ -45,7 +47,7 @@ const styles = {
     justifyContent: 'center',
 
     width: '100vw',
-    height: '460px',
+    height: '600px',
 
     backgroundImage: `url(${landingImage})`,
     backgroundSize: 'cover',
@@ -55,10 +57,10 @@ const styles = {
   },
   landingHeader: {
     textTransform: 'uppercase',
-    letterSpacking: '0.25em',
+    letterSpacing: '0.25em',
   },
   root: {
-    margin: '20px auto',
+    margin: '70px auto',
     width: '80vw',
   },
   gridItem: {
@@ -67,4 +69,6 @@ const styles = {
 };
 
 
-export const HomePage = withStyles(styles)(_HomePage);
+export const HomePage = withTheme()(
+  withStyles(styles)(_HomePage)
+);
