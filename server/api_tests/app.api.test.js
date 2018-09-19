dd7c6686b38bc92320c970e1909341ab0eae9e11
AmandaOh <amandaoh@thoughtworks.com>
2018-09-19 21:41:08 +0800

    [MISC][Amanda] Fix user sign up api tests
    - Mock sign up service to return no errors

--- server/api_tests/app.api.test.js
+++ server/api_tests/app.api.test.js
@@ -2,22 +2,35 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 
 import app from 'app';
+import { SignUpService } from 'services/SignUpService';
+
+jest.mock('services/SignUpService');
 
 beforeAll(async () => {
   await mongoose.connect(global.mongoUri);
+  SignUpService.mockClear();
 });
 
 afterAll(async () => {
   mongoose.disconnect();
 });
 
+const mockSignUpServiceReturnsNoErrors = () => {
+  SignUpService.mockImplementation(() => ({
+    execute: () => {},
+  }));
+};
+
 describe('User sign up route', () => {
   test('post request returns status code 201', async () => {
+    mockSignUpServiceReturnsNoErrors();
+
     const user = {
       name: 'username',
       email: 'email@email.com',
       password: 'xxx',
     };
+
     const response = await request(app).post('/api/v1/users').send({ user });
 
     expect(response.status).toEqual(201);
