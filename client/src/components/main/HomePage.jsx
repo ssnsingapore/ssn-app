4a6eb55497107bbd21d06f6d9e255405b1105961
Sabrina Tjeng <sabriele@users.noreply.github.com>
2018-09-22 05:56:37 +0800

    Implement UI for login/signup form and add buttons according to InVision homepage mockups

--- client/src/components/main/HomePage.jsx
+++ client/src/components/main/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Typography, Grid, Paper } from '@material-ui/core';
+import { Typography, Grid, Paper, Button } from '@material-ui/core';
 import { withStyles, withTheme } from '@material-ui/core/styles';
 
 import { ProjectListing } from 'components/ProjectListing';
@@ -14,28 +14,55 @@ class _HomePage extends Component {
     return (
       <div>
         <div className={classes.landingImage}>
-          <Typography variant="display2" gutterBottom className={classes.landingHeader}>
+          <Typography
+            variant="display2"
+            gutterBottom
+            className={classes.landingHeader}
+          >
             SSN Project Portal
           </Typography>
           <Typography variant="headline">
-            SSN Project Portal aims to match volunteers and organisers in the Singapore Sustainability Space
+            SSN Project Portal aims to match volunteers and organisers in the
+            Singapore Sustainability Space
           </Typography>
         </div>
 
-        <Grid container spacing={4 * theme.spacing.unit} className={classes.root}>
-          <Typography variant="headline" gutterBottom className={classes.marginBottom3}>
+        <Grid
+          container
+          spacing={4 * theme.spacing.unit}
+          className={classes.root}
+        >
+          <Typography
+            variant="headline"
+            gutterBottom
+            className={classes.marginBottom3}
+          >
             Find a Sustainability Project to Volunteer for:
           </Typography>
           <Grid container spacing={4 * theme.spacing.unit}>
             <Grid item md={9} xs={12}>
               <ProjectListing />
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                fullWidth
+                className={classes.button}
+              >
+                View All Projects
+              </Button>
             </Grid>
             <Grid item md={3} xs={12}>
               <Typography variant="headline" component="h3" gutterBottom>
                 Have a project in mind?
               </Typography>
-              <Typography component="p" gutterBottom className={classes.marginBottom3}>
-                Sign up as a project owner to post a project, or login if you already have an account.
+              <Typography
+                component="p"
+                gutterBottom
+                className={classes.marginBottom3}
+              >
+                Sign up as a project owner to post a project, or login if you
+                already have an account.
               </Typography>
               <Paper>
                 <LoginSignup />
@@ -98,7 +125,4 @@ const styles = theme => ({
   },
 });
 
-
-export const HomePage = withTheme()(
-  withStyles(styles)(_HomePage)
-);
+export const HomePage = withTheme()(withStyles(styles)(_HomePage));
