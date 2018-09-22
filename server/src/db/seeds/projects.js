21e3fba4f465ae6c139199af7e38ab6126419001
maryanahermawan <maryanahermawan@gmail.com>
2018-09-22 14:36:09 +0800

    Add enumeration to project schema and project seed

--- server/src/db/seeds/projects.js
+++ server/src/db/seeds/projects.js
@@ -1,125 +1,89 @@
 import { Project } from 'models/Project';
 import {
-  seedData, getProjectOwner, connectMongoose, closeMongooseConnection,
+  seedData,
+  getProjectOwner,
+  connectMongoose,
+  closeMongooseConnection,
 } from './utils';
 
-<<<<<<< HEAD
 connectMongoose();
 
-getProjectOwner().then(async (projectOwner) => {
-  if (!projectOwner) {
-    console.log('Please seed Project Owners first');
-    return;
-  }
-  const volunteerRequirementAttributes = [
-    {
-      type: 'Booth Assistant',
-      commitmentLevel: 'Once a Week',
-      number: 5,
-    },
-  ];
+getProjectOwner()
+  .then(async (projectOwner) => {
+    if (!projectOwner) {
+      console.log('Please seed Project Owners first');
+      return;
+    }
+    const volunteerRequirementAttributes = [
+      {
+        type: '',
+        commitmentLevel: 'Once a Week',
+        number: 5,
+      },
+    ];
 
-  const projectAttributes = [
-    {
-      title: 'Save the Earth',
-      coverImageUrl: 'www.google.com',
-      description: 'Save the earth description',
-      volunteerSignupUrl: '',
-      volunteerRequirements: volunteerRequirementAttributes,
-      projectOwner: projectOwner.id,
-      issuesAddressed: ['BIODIVERSITY'],
-    },
-    {
-      title: 'Cat Adoption Drive',
-      coverImageUrl: 'www.google.com',
-      description: 'Save the earth description',
-      volunteerSignupUrl: '',
-      volunteerRequirements: volunteerRequirementAttributes,
-      projectOwner: projectOwner.id,
-      issuesAddressed: ['AIR_QUALITY'],
-    },
-    {
-      title: 'Greenland Project',
-      coverImageUrl: 'www.google.com',
-      description: 'Greenland Project description',
-      volunteerSignupUrl: '',
-      volunteerRequirements: volunteerRequirementAttributes,
-      projectOwner: projectOwner.id,
-      issuesAddressed: ['BIODIVERSITY', 'AIR_QUALITY'],
-=======
-const volunteerRequirementAttributes = [
-  {
-    type: 'INTERACTION_(HOSTING/MODERATING,_ETC)',
-    commitmentLevel: 'Once a Week',
-    number: 5,
-  },
-];
+    const projectAttributes = [
+      {
+        title: 'Save the Earth',
+        coverImageUrl: 'www.google.com',
+        description: 'Save the earth description',
+        volunteerSignupUrl: '',
+        volunteerRequirements: volunteerRequirementAttributes,
+        projectOwner: projectOwner.id,
+        issuesAddressed: ['BIODIVERSITY'],
+        volunteerRequirementsDescription: 'requirementDescription1',
+        volunteerBenefitsDescription: 'certificate',
+        projectType: ['RECURRING'],
+        time: '9 AM',
+        location: ['CENTRAL'],
+        state: ['PENDING_APPROVAL'],
+        startDate: '1 NOV 2018',
+        endDate: '30 NOV 2018',
+        frequency: ['ONCE_A_WEEK'],
+      },
+      {
+        title: 'Cat Adoption Drive',
+        coverImageUrl: 'www.google.com',
+        description: 'Save the earth description',
+        volunteerSignupUrl: '',
+        volunteerRequirements: volunteerRequirementAttributes,
+        projectOwner: projectOwner.id,
+        issuesAddressed: ['AIR_QUALITY'],
+        volunteerRequirementsDescription: 'requirementDescription2',
+        volunteerBenefitsDescription: 'lunch',
+        projectType: ['EVENT'],
+        time: '10 AM',
+        location: ['WEST'],
+        state: ['PENDING_APPROVAL'],
+        startDate: '1 DEC 2018',
+        endDate: '30 DEC 2018',
+        frequency: ['ONCE_A_WEEK'],
+      },
+      {
+        title: 'Greenland Project',
+        coverImageUrl: 'www.google.com',
+        description: 'Greenland Project description',
+        volunteerSignupUrl: '',
+        volunteerRequirements: volunteerRequirementAttributes,
+        projectOwner: projectOwner.id,
+        issuesAddressed: ['WASTE', 'FOOD_&_AGRICULTURE'],
+        volunteerRequirementsDescription: 'requirementDescription1',
+        volunteerBenefitsDescription: 'certificate',
+        projectType: ['RECURRING'],
+        time: '9 AM',
+        location: ['EAST'],
+        state: ['PENDING_APPROVAL'],
+        startDate: '1 JAN 2019',
+        endDate: '30 JAN 2019',
+        frequency: ['A_FEW_TIMES_A_YEAR'],
+      },
+    ];
 
-const projectAttributes = [
-  {
-    title: 'Save the Earth',
-    coverImageUrl: 'www.google.com',
-    description: 'Save the earth description',
-    volunteerSignupUrl: '',
-    volunteerRequirements: volunteerRequirementAttributes,
-    projectOwner: '5b9dc395eaf06391d3df9a90',
-    issuesAddressed: ['BIODIVERSITY'],
-    volunteerRequirementsDescription: 'requirementDescription1',
-    volunteerBenefitsDescription: 'certificate',
-    projectType: ['RECURRING'],
-    time: '9 AM',
-    location: ['CENTRAL'],
-    state: ['PENDING_APPROVAL'],
-    startDate: '1 NOV 2018',
-    endDate: '30 NOV 2018',
-    frequency: ['ONCE_A_WEEK'],
-  },
-  {
-    title: 'Cat Adoption Drive',
-    coverImageUrl: 'www.google.com',
-    description: 'Save the earth description',
-    volunteerSignupUrl: '',
-    volunteerRequirements: volunteerRequirementAttributes,
-    projectOwner: '5b9dc395eaf06391d3df9a90',
-    issuesAddressed: ['AIR_QUALITY'],
-    volunteerRequirementsDescription: 'requirementDescription2',
-    volunteerBenefitsDescription: 'lunch',
-    projectType: ['EVENT'],
-    time: '10 AM',
-    location: ['WEST'],
-    state: ['PENDING_APPROVAL'],
-    startDate: '1 DEC 2018',
-    endDate: '30 DEC 2018',
-    frequency: ['ONCE_A_WEEK'],
-  },
-  {
-    title: 'Greenland Project',
-    coverImageUrl: 'www.google.com',
-    description: 'Greenland Project description',
-    volunteerSignupUrl: '',
-    volunteerRequirements: volunteerRequirementAttributes,
-    projectOwner: '5b9dc395eaf06391d3df9a90',
-    issuesAddressed: ['WASTE', 'FOOD_&_AGRICULTURE'],
-    volunteerRequirementsDescription: 'requirementDescription1',
-    volunteerBenefitsDescription: 'certificate',
-    projectType: ['RECURRING'],
-    time: '9 AM',
-    location: ['EAST'],
-    state: ['PENDING_APPROVAL'],
-    startDate: '1 JAN 2019',
-    endDate: '30 JAN 2019',
-    frequency: ['A_FEW_TIMES_A_YEAR'],
-  },
-];
->>>>>>> server/src/models/Project.js
-
-    },
-  ];
-
-  await seedData(projectAttributes, Project, 'project');
-}).catch((err) => {
-  console.log(`error: ${err}`);
-})
+    await seedData(projectAttributes, Project, 'project');
+  })
+  .catch((err) => {
+    console.log(`error: ${err}`);
+  })
   .finally(() => {
     closeMongooseConnection();
   });
--- server/src/models/Project.js
+++ server/src/models/Project.js
@@ -1,64 +1,64 @@
 import mongoose from 'mongoose';
 
 export const IssueAddressed = {
-  AIR_QUALITY: 'Air Quality',
-  AWARENESS_AND_EDUCATION: 'Awareness and Education',
-  BIODIVERSITY: 'Biodiversity',
-  CLIMATE: 'Climate',
-  CONSERVATION: 'Conservation',
-  ENERGY: 'Energy',
-  FOOD_AND_AGRICULTURE: 'Food and Agriculture',
-  GREEN_LIFESTYLE: 'Green Lifestyle',
-  LAND_AND_NOISE_POLLUTION: 'Land and Noise Pollution',
-  PLANNING_AND_TRANSPORTATION: 'Planning and Transportation',
-  PRODUCTION_AND_CONSUMPTION: 'Production and Consumption',
-  OTHER: 'Other',
-  SPORTS_AND_RECREATION: 'Sports and Recreation',
-  WASTE: 'Waste',
-  WATER: 'Water',
-  GREEN_TECHNOLOGY: 'Green Technology',
+  AIR_QUALITY: 'AIR_QUALITY',
+  AWARENESS_AND_EDUCATION: 'AWARENESS_AND_EDUCATION',
+  BIODIVERSITY: 'BIODIVERSITY',
+  CLIMATE: 'CLIMATE',
+  CONSERVATION: 'CONSERVATION',
+  ENERGY: 'ENERGY',
+  FOOD_AND_AGRICULTURE: 'FOOD_AND_AGRICULTURE',
+  GREEN_LIFESTYLE: 'GREEN_LIFESTYLE',
+  LAND_AND_NOISE_POLLUTION: 'LAND_AND_NOISE_POLLUTION',
+  PLANNING_AND_TRANSPORTATION: 'PLANNING_AND_TRANSPORTATION',
+  PRODUCTION_AND_CONSUMPTION: 'PRODUCTION_AND_CONSUMPTION',
+  OTHER: 'OTHER',
+  SPORTS_AND_RECREATION: 'SPORTS_AND_RECREATION',
+  WASTE: 'WASTE',
+  WATER: 'WATER',
+  GREEN_TECHNOLOGY: 'GREEN_TECHNOLOGY',
 };
 
 export const ProjectState = {
-  PENDING_APPROVAL: 'Pending Approval',
-  APPROVED_ACTIVE: 'Approved Active',
-  APPROVED_INACTIVE: 'Approved Inactive',
-  REJECTED: 'Rejected',
+  PENDING_APPROVAL: 'PENDING_APPROVAL',
+  APPROVED_ACTIVE: 'APPROVED_ACTIVE',
+  APPROVED_INACTIVE: 'APPROVED_INACTIVE',
+  REJECTED: 'REJECTED',
 };
 
 export const ProjectType = {
-  EVENT: 'Event',
-  RECURRING: 'Recurring',
+  EVENT: 'EVENT',
+  RECURRING: 'RECURRING',
 };
 
 export const ProjectLocation = {
-  CENTRAL: 'Central',
-  NORTH: 'North',
-  SOUTH: 'South',
-  EAST: 'East',
-  WEST: 'West',
+  CENTRAL: 'CENTRAL',
+  NORTH: 'NORTH',
+  SOUTH: 'SOUTH',
+  EAST: 'EAST',
+  WEST: 'WEST',
 };
 
 export const ProjectFrequency = {
-  EVERY_DAY: 'Every Day',
-  A_FEW_TIMES_A_WEEK: 'A Few Times a Week',
-  ONCE_A_WEEK: 'Once a Week',
-  FORTNIGHTLY: 'Fortnightly',
-  A_FEW_TIMES_A_MONTH: 'A Few Times a Month',
-  ONCE_A_MONTH: 'Once a Month',
-  A_FEW_TIMES_A_YEAR: 'A Few Times a Year',
-  ONCE_A_YEAR: 'Once a Year',
+  EVERY_DAY: 'EVERY_DAY',
+  A_FEW_TIMES_A_WEEK: 'A_FEW_TIMES_A_WEEK',
+  ONCE_A_WEEK: 'ONCE_A_WEEK',
+  FORTNIGHTLY: 'FORTNIGHTLY',
+  A_FEW_TIMES_A_MONTH: 'A_FEW_TIMES_A_MONTH',
+  ONCE_A_MONTH: 'ONCE_A_MONTH',
+  A_FEW_TIMES_A_YEAR: 'A_FEW_TIMES_A_YEAR',
+  ONCE_A_YEAR: 'ONCE_A_YEAR',
 };
 
 export const VolunteerRequirementType = {
-  INTERACTION: 'Interaction (Hosting / Moderating, etc)',
-  CONTENT_CREATION: 'Content Creation (Design, Writing, etc)',
-  EVENT_PLANNING: 'Event Planning (Logistics, Programme, etc)',
-  MEDIA_AND_SOCIAL_MEDIA: 'Media & Social Media (Photography, Publicity, etc',
-  EXPERT_VOLUNTEERS: 'Expert Volunteers (Technologist, Mentors, etc',
-  ADHOC_MANPOWER_SUPPORT: 'Ad-Hoc Manpower Support (Ushering, Administrative Work, Working Booths, etc)',
-  OTHERS_SKILLED: 'Others (Skilled)',
-  OTHERS_ADHOC: 'Others (Ad-hoc)',
+  INTERACTION: 'INTERACTION',
+  CONTENT_CREATION: 'CONTENT_CREATION',
+  EVENT_PLANNING: 'EVENT_PLANNING',
+  MEDIA_AND_SOCIAL_MEDIA: 'MEDIA_AND_SOCIAL_MEDIA',
+  EXPERT_VOLUNTEERS: 'EXPERT_VOLUNTEERS',
+  ADHOC_MANPOWER_SUPPORT: 'ADHOC_MANPOWER_SUPPORT',
+  OTHERS_SKILLED: 'OTHERS_SKILLED',
+  OTHERS_ADHOC: 'OTHERS_ADHOC',
 };
 
 const VolunteerRequirementSchema = new mongoose.Schema({
@@ -112,16 +112,22 @@ const ProjectSchema = new mongoose.Schema(
       enum: Object.values(ProjectState),
     },
     startDate: {
-      type: String,
-      required: [true, 'cannot be blank'], // only if event
+      type: Date,
+      required: [function () {
+        return (this.projectType === ProjectType.EVENT);
+      }, 'cannot be blank'],
     },
     endDate: {
-      type: String,
-      required: [true, 'cannot be blank'], // only if event
+      type: Date,
+      required: [function () {
+        return (this.projectType === ProjectType.EVENT);
+      }, 'cannot be blank'],
     },
     frequency: {
       type: String,
-      required: [true, 'cannot be blank'], // only if recurring
+      required: [function () {
+        return (this.projectType === ProjectType.RECURRING);
+      }, 'cannot be blank'],
       enum: Object.values(ProjectFrequency),
     },
   },
