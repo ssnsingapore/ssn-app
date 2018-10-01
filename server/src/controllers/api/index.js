11c9dc564dc8b498a0c4100473ac3e985dab7f8f
Eunice Soh <t0915817@kelaberetiv.sg>
2018-10-01 22:02:21 +0800

    [SSN-7][ES] Backend route for a single project by id

--- server/src/controllers/api/index.js
+++ server/src/controllers/api/index.js
@@ -4,7 +4,7 @@ import { adminsRouter } from './admins_controller';
 import { usersRouter } from './users_controller';
 import { todosRouter } from './todos_controller';
 import { imagesRouter } from './images_controller';
-import { projectsRouterNumber } from './projects_controller';
+import { projectsRouterNumber, projectRouter } from './projects_controller';
 import { projectOwnersRouter } from './project_owners_controller';
 
 export const apiRouter = express.Router();
@@ -16,4 +16,5 @@ apiRouter.use(todosRouter);
 // apiRouter.use(projectsRouterHome);
 // apiRouter.use(projectsRouterSearch);
 apiRouter.use(projectsRouterNumber);
+apiRouter.use(projectRouter);
 apiRouter.use(projectOwnersRouter);
