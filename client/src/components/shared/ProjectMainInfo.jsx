cf4f8455368b1a68161388f48e4396961704efc9
sabriele <sabriele@users.noreply.github.com>
2018-10-20 04:18:19 +0800

    [SSN-34][Sab] Convert dumb components (class to functional)

--- client/src/components/shared/ProjectMainInfo.jsx
+++ client/src/components/shared/ProjectMainInfo.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import {
   Grid,
@@ -23,174 +23,149 @@ const renderRow = (label, value) => (
   </Typography>
 );
 
-class _ProjectMainInfo extends Component {
-  renderIssuesAddressed = () => {
-    const { classes } = this.props;
-    const { issuesAddressed } = this.props.project;
+const renderIssuesAddressed = (classes, project) => {
+  const { issuesAddressed } = project;
 
-    return (
-      <React.Fragment>
-        {issuesAddressed.map(issueAddressed => {
-          return (
-            <Chip
-              key={issueAddressed}
-              label={issueAddressed}
-              className={classes.chip}
-            />
-          );
-        })}
-      </React.Fragment>
-    );
-  };
+  return (
+    <React.Fragment>
+      {issuesAddressed.map(issueAddressed => {
+        return (
+          <Chip
+            key={issueAddressed}
+            label={issueAddressed}
+            className={classes.chip}
+          />
+        );
+      })}
+    </React.Fragment>
+  );
+};
 
-  renderProjectBaseDetails = () => {
-    const { classes } = this.props;
-    const {
-      title,
-      description,
-      volunteerSignupUrl,
-      coverImageUrl,
-    } = this.props.project;
+const renderProjectBaseDetails = (classes, project) => {
+  const { title, description, volunteerSignupUrl, coverImageUrl } = project;
 
-    return (
-      <Card className={classes.card} square>
-        <CardContent className={classes.content}>
-          <Typography
-            variant="headline"
-            gutterBottom
-            className={classes.headline}
-          >
-            {title}
-          </Typography>
-          <Typography
-            variant="body1"
-            gutterBottom
-            style={{ marginBottom: '60px' }}
-          >
-            {description}
-          </Typography>
-          <Button
-            variant="contained"
-            href={volunteerSignupUrl}
-            fullWidth
-            color="secondary"
-            size="large"
-          >
-            Sign up as a volunteer!
-          </Button>
-        </CardContent>
-        <CardMedia className={classes.cover} image={coverImageUrl} />
-      </Card>
-    );
-  };
-
-  renderVolunteerDetails = () => {
-    const { classes } = this.props;
-    const {
-      volunteerRequirements,
-      volunteerRequirementsDescription,
-      volunteerBenefitsDescription,
-    } = this.props.project;
-
-    return (
-      <Paper className={classes.paper} square>
+  return (
+    <Card className={classes.card} square>
+      <CardContent className={classes.content}>
         <Typography
           variant="headline"
           gutterBottom
           className={classes.headline}
         >
-          Volunteer Details
+          {title}
         </Typography>
-        <Typography variant="body2">
-          We need the following volunteers:
-        </Typography>
-        <div className={classes.tableWrapper}>
-          <Table className={classes.table}>
-            <TableHead>
-              <TableRow>
-                <TableCell>Volunteer Roles</TableCell>
-                <TableCell numeric>No. of Volunteers</TableCell>
-                <TableCell numeric>Commitment Level</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {volunteerRequirements.map(requirement => {
-                return (
-                  <TableRow key={requirement._id}>
-                    <TableCell component="th" scope="row">
-                      {requirement.type}
-                    </TableCell>
-                    <TableCell numeric>
-                      {requirement.number} volunteers
-                    </TableCell>
-                    <TableCell numeric>{requirement.commitmentLevel}</TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-          <Typography variant="body1">
-            {volunteerRequirementsDescription}
-          </Typography>
-        </div>
-        <Typography variant="body2">Volunteer benefits:</Typography>
-        <Typography variant="body1">{volunteerBenefitsDescription}</Typography>
-      </Paper>
-    );
-  };
-
-  renderProjectDetails = () => {
-    const { classes } = this.props;
-    const {
-      startDate,
-      endDate,
-      time,
-      location,
-      projectType,
-      frequency,
-    } = this.props.project;
-
-    return (
-      <Paper className={classes.paper} square>
         <Typography
-          variant="headline"
+          variant="body1"
           gutterBottom
-          className={classes.headline}
+          style={{ marginBottom: '60px' }}
         >
-          Project Details
+          {description}
         </Typography>
-        {renderRow('Start date:', startDate)}
-        {renderRow('End date:', endDate)}
-        {renderRow('Time:', time)}
-        {renderRow('Location:', location)}
-        {renderRow('Project Type:', projectType)}
-        {renderRow('Frequency:', frequency)}
-        {renderRow('Issues Addressed:')}
-        {this.renderIssuesAddressed()}
-      </Paper>
-    );
-  };
+        <Button
+          variant="contained"
+          href={volunteerSignupUrl}
+          fullWidth
+          color="secondary"
+          size="large"
+        >
+          Sign up as a volunteer!
+        </Button>
+      </CardContent>
+      <CardMedia className={classes.cover} image={coverImageUrl} />
+    </Card>
+  );
+};
 
-  render() {
-    const { classes } = this.props;
+const renderVolunteerDetails = (classes, project) => {
+  const {
+    volunteerRequirements,
+    volunteerRequirementsDescription,
+    volunteerBenefitsDescription,
+  } = project;
 
-    return (
-      <div className={classes.root}>
-        <Grid container spacing={16}>
-          <Grid item xs={12}>
-            {this.renderProjectBaseDetails()}
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            {this.renderVolunteerDetails()}
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            {this.renderProjectDetails()}
-          </Grid>
-        </Grid>
+  return (
+    <Paper className={classes.paper} square>
+      <Typography variant="headline" gutterBottom className={classes.headline}>
+        Volunteer Details
+      </Typography>
+      <Typography variant="body2">We need the following volunteers:</Typography>
+      <div className={classes.tableWrapper}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Volunteer Roles</TableCell>
+              <TableCell numeric>No. of Volunteers</TableCell>
+              <TableCell numeric>Commitment Level</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {volunteerRequirements.map(requirement => {
+              return (
+                <TableRow key={requirement._id}>
+                  <TableCell component="th" scope="row">
+                    {requirement.type}
+                  </TableCell>
+                  <TableCell numeric>{requirement.number} volunteers</TableCell>
+                  <TableCell numeric>{requirement.commitmentLevel}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+        <Typography variant="body1">
+          {volunteerRequirementsDescription}
+        </Typography>
       </div>
-    );
-  }
-}
+      <Typography variant="body2">Volunteer benefits:</Typography>
+      <Typography variant="body1">{volunteerBenefitsDescription}</Typography>
+    </Paper>
+  );
+};
+
+const renderProjectDetails = (classes, project) => {
+  const {
+    startDate,
+    endDate,
+    time,
+    location,
+    projectType,
+    frequency,
+  } = project;
+
+  return (
+    <Paper className={classes.paper} square>
+      <Typography variant="headline" gutterBottom className={classes.headline}>
+        Project Details
+      </Typography>
+      {renderRow('Start date:', startDate)}
+      {renderRow('End date:', endDate)}
+      {renderRow('Time:', time)}
+      {renderRow('Location:', location)}
+      {renderRow('Project Type:', projectType)}
+      {renderRow('Frequency:', frequency)}
+      {renderRow('Issues Addressed:')}
+      {renderIssuesAddressed(classes, project)}
+    </Paper>
+  );
+};
+
+const _ProjectMainInfo = ({ classes, project }) => {
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={16}>
+        <Grid item xs={12}>
+          {renderProjectBaseDetails(classes, project)}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          {renderVolunteerDetails(classes, project)}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          {renderProjectDetails(classes, project)}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 const styles = theme => ({
   root: {},
