875dcd6c3df9d00794efbde5383e14b5f6fbbd86
sabriele <sabriele@users.noreply.github.com>
2018-10-26 16:56:42 +0800

    [SSN-36][Sab] Add route for Edit Project Preview

--- client/src/components/main/Routes.jsx
+++ client/src/components/main/Routes.jsx
@@ -19,6 +19,7 @@ import { AwaitingAccountConfirmation } from 'components/public/AwaitingAccountCo
 
 import { ProjectOwnerDashboard } from 'components/project_owner/ProjectOwnerDashboard';
 import { ProjectOwnerNewProjectForm } from 'components/project_owner/new_project_form/ProjectOwnerNewProjectForm';
+import { EditProjectPreview } from 'components/project_owner/EditProjectPreview';
 
 import { AdminDashboard } from 'components/admin/AdminDashboard';
 import { AdminProjectDetails } from 'components/admin/admin_project_details/AdminProjectDetails';
@@ -66,6 +67,11 @@ class _Routes extends Component {
                 component={ProjectOwnerNewProjectForm}
                 authorize={[Role.PROJECT_OWNER]}
               />
+              <RouteAuthenticated
+                path="/project_owner/projects/:id"
+                component={EditProjectPreview}
+                authorize={[Role.PROJECT_OWNER]}
+              />
 
               <Route exact path="/admin" component={AdminLoginPage} />
               <RouteAuthenticated
@@ -75,9 +81,9 @@ class _Routes extends Component {
                 authorize={[Role.ADMIN]}
               />
 
-              <RouteAuthenticated 
-                exact 
-                path="/admin/projects/:id" 
+              <RouteAuthenticated
+                exact
+                path="/admin/projects/:id"
                 component={AdminProjectDetails}
                 authorize={[Role.ADMIN]}
               />
