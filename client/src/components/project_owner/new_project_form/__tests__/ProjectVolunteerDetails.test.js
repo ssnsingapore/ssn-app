5f1d64f74af6f3b9ee57ef27982dd0edb9d702d0
AmandaOh <amandaoh@thoughtworks.com>
2018-10-27 22:49:56 +0800

    [SSN-19][Amanda] Add tests for project volunteer details component

---
+++ client/src/components/project_owner/new_project_form/__tests__/ProjectVolunteerDetails.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import { ProjectVolunteerDetails } from '../ProjectVolunteerDetails';
+import { VolunteerRequirementForm } from '../VolunteerRequirementForm';
+
+
+describe('ProjectVolunteerDetails', () => {
+  let component, FieldName, fields;
+  
+  beforeEach(() => {
+    FieldName = {
+      volunteerRequirementsDescription: 'volunteerRequirementsDescription',
+      volunteerBenefitsDescription: 'volunteerBenefitsDescription',
+    };
+
+    fields = {
+      [FieldName.volunteerRequirementsDescription]: {
+        value: 'description for volunteer requirements',
+        errors: [],
+      },
+      [FieldName.volunteerBenefitsDescription]: {
+        value: 'description for volunteer benefits',
+        errors: [], 
+      },
+    };
+
+    const props = {
+      fields,
+      FieldName,
+      volunteerRequirementRefs: [React.createRef(), React.createRef()],
+    };
+
+    component = mount(<ProjectVolunteerDetails {...props} />);
+  });
+
+  it('has Volunteer Details as its heading', () => {
+    expect(component.find('Typography').text()).toEqual('Volunteer Details');
+  });
+
+  it('has text field for description of volunteers needed', () => {
+    expect(component.find('TextField').get(0).props.label).toEqual('Description of Volunteers Needed');
+    expect(component.find('TextField').get(0).props.name).toEqual(FieldName.volunteerRequirementsDescription);
+    expect(component.find('TextField').get(0).props.value).toEqual(fields[FieldName.volunteerRequirementsDescription].value);
+  });
+
+  it('has text field for description of volunteer benefits', () => {
+    const volunteerBenefitsField = component.find('TextField').filterWhere(field => field.props().id === FieldName.volunteerBenefitsDescription);
+
+    expect(volunteerBenefitsField.props().label).toEqual('Volunteer Benefits');
+    expect(volunteerBenefitsField.props().name).toEqual(FieldName.volunteerBenefitsDescription);
+    expect(volunteerBenefitsField.props().value).toEqual(fields[FieldName.volunteerBenefitsDescription].value);
+  });
+
+  it('renders correct number of volunteer requirement rows', () => {
+    expect(component.find(VolunteerRequirementForm)).toHaveLength(2);
+  });
+});
