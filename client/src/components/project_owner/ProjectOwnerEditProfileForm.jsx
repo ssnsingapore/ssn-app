d129dc35c9a16ca513e0beec606f3ae03667c219
Eunice Soh <t0915817@kelaberetiv.sg>
2018-11-04 21:13:17 +0800

    [SSN-61][ES] Allow project owner to add upload a new profile photo.

---
+++ client/src/components/project_owner/ProjectOwnerEditProfileForm.jsx
@@ -0,0 +1,210 @@
+import { withTheme } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import { withContext } from 'util/context';
+import { extractErrors, formatErrors } from 'util/errors';
+import { getFieldNameObject, withForm } from 'util/form';
+
+import { AccountType } from 'components/shared/enums/AccountType';
+import { AlertType } from 'components/shared/Alert';
+import { AppContext } from 'components/main/AppContext';
+import { ProjectOwnerBaseProfileForm } from 'components/shared/ProjectOwnerBaseProfileForm';
+import { Spinner } from 'components/shared/Spinner';
+
+
+export const PROJECT_OWNER_PROFILE_DISPLAY_WIDTH = 200;
+export const PROJECT_OWNER_PROFILE_DISPLAY_HEIGHT = 200;
+
+const DISPLAY_WIDTH = PROJECT_OWNER_PROFILE_DISPLAY_WIDTH;
+const DISPLAY_HEIGHT = PROJECT_OWNER_PROFILE_DISPLAY_HEIGHT;
+
+const EDIT_PROFILE_SUCCESS = 'You\'ve successfully updated your profile!';
+const FieldName = getFieldNameObject([
+  'email',
+  'name',
+  'accountType',
+  'organisationName',
+  'websiteUrl',
+  'socialMediaLink',
+  'description',
+  'personalBio',
+]);
+
+const constraints = {
+  [FieldName.name]: {
+    presence: { allowEmpty: false },
+    length: { maximum: 50 },
+  },
+  [FieldName.email]: {
+    presence: { allowEmpty: false },
+    email: true,
+  }, [FieldName.accountType]: {
+    presence: { allowEmpty: false },
+    inclusion: Object.values(AccountType),
+  },
+  [FieldName.websiteUrl]: {
+    isUrl: { allowEmpty: true },
+  },
+  [FieldName.organisationName]: (value, attributes) => {
+    if (attributes.accountType === AccountType.INDIVIDUAL) return null;
+
+    return {
+      presence: { allowEmpty: false },
+    };
+  },
+};
+
+class _ProjectOwnerEditProfileForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.profilePhotoInput = React.createRef();
+
+    this.state = {
+      isSubmitting: false,
+      isLoading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { requestWithAlert } = this.props.context.utils;
+    const { authenticator } = this.props.context.utils;
+    const currentUser = authenticator.getCurrentUser();
+
+    const endPoint = `/api/v1/project_owners/${currentUser.id}`;
+    const response = await requestWithAlert.get(endPoint,
+      { authenticated: true });
+
+    if (response.isSuccessful) {
+      const projectOwner = (await response.json()).projectOwner;
+      Object.keys(projectOwner).map(data => this.props.setField(data, projectOwner[data]));
+    }
+
+    if (response.hasError) {
+      const { showAlert } = this.props.context.updaters;
+      const errors = await extractErrors(response);
+      showAlert('getProjectOwnerFailure', AlertType.ERROR, formatErrors(errors));
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    if (this.profilePhotoInput.current.files[0]) {
+      const resizedProfilePhoto = await this.resizeImage();
+      formData.append('profilePhoto', resizedProfilePhoto);
+    }
+
+    if (this.state.isImageTooLowResolution || !this.props.validateAllFields()) {
+      return;
+    }
+
+    const projectOwner = { ...this.props.valuesForAllFields() };
+    Object.keys(projectOwner)
+      .filter(key => projectOwner[key] !== undefined)
+      .forEach(key => formData.append(key, projectOwner[key]));
+
+    const { showAlert } = this.props.context.updaters;
+    const { requestWithAlert, authenticator } = this.props.context.utils;
+
+    const projectOwner = { ...this.props.valuesForAllFields() };
+
+    this.setState({ isSubmitting: true });
+
+    const currentUser = authenticator.getCurrentUser();
+    const data = { projectOwner };
+
+    const response = await requestWithAlert
+      .put(`/api/v1/project_owners/${currentUser.id}`, data, { authenticated: true });
+
+    this.setState({ isSubmitting: false });
+
+    if (response.isSuccessful) {
+      const newUser = (await response.json()).projectOwner;
+      authenticator.setCurrentUser(newUser);
+      showAlert('editProfileSuccess', AlertType.SUCCESS, EDIT_PROFILE_SUCCESS);
+      window.location.reload();
+    }
+
+    if (response.hasError) {
+      const errors = await extractErrors(response);
+      showAlert('editProfileFailure', AlertType.ERROR, formatErrors(errors));
+    }
+
+  }
+
+  resizeImage = async () => {
+    const image = new Image();
+
+    const profilePhoto = this.profilePhotoInput.current.files[0];
+    const profilePhotoSrc = window.URL.createObjectURL(profilePhoto);
+
+    image.src = profilePhotoSrc;
+
+    // Resize to cover the display width and height
+    const { width, height } = await this.getImageDimensions(image);
+
+    if (width < DISPLAY_WIDTH || height < DISPLAY_HEIGHT) {
+      this.setState({ isImageTooLowResolution: true });
+    } else {
+      this.setState({ isImageTooLowResolution: false });
+    }
+
+    let finalWidth = width;
+    let finalHeight = height;
+
+    if (width < height && width > DISPLAY_WIDTH) {
+      finalWidth = DISPLAY_WIDTH;
+      finalHeight = height / width * DISPLAY_WIDTH;
+    }
+
+    if (height < width && height > DISPLAY_HEIGHT) {
+      finalHeight = DISPLAY_HEIGHT;
+      finalWidth = width / height * DISPLAY_HEIGHT;
+    }
+
+    const canvas = document.createElement('canvas');
+    canvas.width = finalWidth;
+    canvas.height = finalHeight;
+
+    const context = canvas.getContext('2d');
+    context.drawImage(image, 0, 0, finalWidth, finalHeight);
+    return new Promise((resolve) => {
+      canvas.toBlob((blob) => resolve(blob), 'image/jpeg', 0.6);
+    });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <Spinner />;
+    }
+
+    return (
+      <ProjectOwnerBaseProfileForm
+        profilePhotoInput={this.profilePhotoInput}
+        FieldName={FieldName}
+        fields={this.props.fields}
+        handleChange={this.props.handleChange}
+        handleSubmit={this.handleSubmit}
+        isSubmitting={this.state.isSubmitting}
+        isEditProfileForm={true} />
+    );
+  }
+}
+
+export const ProjectOwnerEditProfileForm =
+  withTheme()(
+    withContext(AppContext)(
+      withForm(
+        FieldName,
+        constraints,
+      )(_ProjectOwnerEditProfileForm)
+    ),
+  );
+
+export const TestProjectOwnerEditProfileForm = withForm(
+  FieldName,
+  constraints,
+)(_ProjectOwnerEditProfileForm);
