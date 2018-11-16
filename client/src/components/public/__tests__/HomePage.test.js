5f581734f71d1531103e5bf67d2853602b829f62
AmandaOh <amandaoh@thoughtworks.com>
2018-11-16 22:52:08 +0800

    [MISC][Amanda] Fix HomePage tests

--- client/src/components/public/__tests__/HomePage.test.js
+++ client/src/components/public/__tests__/HomePage.test.js
@@ -1,26 +1,39 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 
-import { HomePage } from '../HomePage';
-import { HomepageProjectListing } from '../../public/HomepageProjectListing';
+import { _HomePage } from '../HomePage';
+import { PublicProjectListing } from 'components/shared/PublicProjectListing';
 import { ProjectOwnerLoginForm } from '../ProjectOwnerLoginForm';
+import { defaultAppContext } from 'components/main/AppContext';
 
-describe('Home page', () => {
+describe('HomePage', () => {
   let component;
 
   beforeEach(() => {
-    component = shallow(<HomePage></HomePage>).dive().dive();
+    const mockTheme = {
+      spacing: {
+        unit: 1,
+      },
+    };
+    const mockStyles = {};
+    defaultAppContext.utils.requestWithAlert = {
+      get: jest.fn(() => Promise.resolve({})),
+    };
+    const mockContext = {
+      ...defaultAppContext,
+    };
+
+    const HomePage = withStyles(mockStyles)(_HomePage);
+    component = shallow(<HomePage theme={mockTheme} context={mockContext} />).dive();
   });
 
   it('should have a list of projects, a button to view all projects, and a login form', () => {
-    // console.log('DEBUG', component.debug());
-    // console.log('DEBUG', component.find(Button).debug());
-
     const buttonProps = component.find(Button).props();
 
     expect(buttonProps.to).toEqual('/projects');
     expect(buttonProps.children).toEqual('View All Projects');
-    expect(component.find(HomepageProjectListing).exists()).toBeTruthy();
+    expect(component.find(PublicProjectListing).exists()).toBeTruthy();
     expect(component.find(ProjectOwnerLoginForm).exists()).toBeTruthy();
   });
 });
