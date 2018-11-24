bb3ebaa184393d091a4a684441d5f60d9ddd30db
maryanahermawan <maryanahermawan@gmail.com>
2018-11-24 12:10:20 +0800

    [SSN-58-61][Amanda|MH] Bug in /projects getProjects sort comes from page limit.

--- server/src/controllers/api/projects_controller.js
+++ server/src/controllers/api/projects_controller.js
@@ -44,8 +44,8 @@ async function getProjects(req, res) {
   } = req.query;
 
   const sortParams = projectState === ProjectState.PENDING_APPROVAL
-    ? { createdAt: 'ascending' }
-    : { updatedAt: 'descending' };
+    ? { createdAt: 1 }
+    : { updatedAt: -1 };
 
   const recurringWithinMonthObj = {
     $and: [
@@ -53,7 +53,7 @@ async function getProjects(req, res) {
       {
         frequency: {
           $in: [ProjectFrequency.EVERY_DAY, ProjectFrequency.A_FEW_TIMES_A_WEEK, ProjectFrequency.ONCE_A_WEEK,
-            ProjectFrequency.FORTNIGHTLY, ProjectFrequency.A_FEW_TIMES_A_MONTH, ProjectFrequency.ONCE_A_MONTH],
+          ProjectFrequency.FORTNIGHTLY, ProjectFrequency.A_FEW_TIMES_A_MONTH, ProjectFrequency.ONCE_A_MONTH],
         },
       },
     ],
@@ -119,10 +119,12 @@ async function getProjects(req, res) {
         } : { state: ProjectState.APPROVED_ACTIVE },
     },
     {
+      $sort: sortParams,
+    },
+    {
       $limit: pageSize,
     },
   ])
-    .sort(sortParams)
     .exec();
   const projects = await ProjectOwner.populate(aggrProjects, { path: 'projectOwner' });
 
@@ -140,7 +142,7 @@ async function getProjectCounts(req, res) {
       {
         frequency: {
           $in: [ProjectFrequency.EVERY_DAY, ProjectFrequency.A_FEW_TIMES_A_WEEK, ProjectFrequency.ONCE_A_WEEK,
-            ProjectFrequency.FORTNIGHTLY, ProjectFrequency.A_FEW_TIMES_A_MONTH, ProjectFrequency.ONCE_A_MONTH],
+          ProjectFrequency.FORTNIGHTLY, ProjectFrequency.A_FEW_TIMES_A_MONTH, ProjectFrequency.ONCE_A_MONTH],
         },
       },
     ],
@@ -207,7 +209,6 @@ async function getProjectCounts(req, res) {
       },
     ])
       .exec();
-    console.log(aggrCountProjects);
     counts[projectStates[i]] = aggrCountProjects.length !== 0 ? aggrCountProjects[0].count : 0;
   }
 
