c329e04e61c1a83ec219ce5c6ea7d06204af2819
tjjjwxzq <aquietimmanence@gmail.com>
2019-01-16 23:40:54 +0800

    [SSN-137][Jun Qi] Unregister service worker to prevent undesired caching

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/main/App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
+// registerServiceWorker();
