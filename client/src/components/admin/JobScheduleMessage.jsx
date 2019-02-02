0111fa5e5731901eb5848a91ce13d84aba99d492
missa4892 <missa4892@gmail.com>
2019-02-02 15:16:06 +0800

    [Sal] Changed cron job error message

--- client/src/components/admin/JobScheduleMessage.jsx
+++ client/src/components/admin/JobScheduleMessage.jsx
@@ -6,14 +6,14 @@ import { amber, red } from '@material-ui/core/colors';
 import { AppContext } from 'components/main/AppContext';
 import { withContext } from 'util/context';
 
-export const errorMessage = 'There is no running job to check for inactive projects.' + 
-' If this configuration was not removed, please check if cron syntax is correct.';
+export const errorMessage = 'There is no running job to check for inactive projects.' +
+  ' Check if CRON_SCHEDULE environment variable is set correctly.';
 export class _JobScheduleMessage extends Component {
   state = {
     nextJobRunTime: '',
     errorMessage: '',
   };
-  
+
   async componentDidMount() {
     const { requestWithAlert } = this.props.context.utils;
     const endpoint = '/api/v1/admins/cron_job_time';
@@ -32,22 +32,22 @@ export class _JobScheduleMessage extends Component {
   }
 
   render() {
-    const { classes } = this.props; 
+    const { classes } = this.props;
 
     return (
       <React.Fragment>
         {this.state.errorMessage &&
-        <div className={classes.error}>
-          <Typography>
-            {this.state.errorMessage}
-          </Typography>
-        </div>}
-        {this.state.nextJobRunTime && 
-        <div className={classes.root}>
-          <Typography>
-            Projects past their event end dates will become inactive on  {this.state.nextJobRunTime}
-          </Typography>
-        </div>}
+          <div className={classes.error}>
+            <Typography>
+              {this.state.errorMessage}
+            </Typography>
+          </div>}
+        {this.state.nextJobRunTime &&
+          <div className={classes.root}>
+            <Typography>
+              Projects past their event end dates will become inactive on  {this.state.nextJobRunTime}
+            </Typography>
+          </div>}
       </React.Fragment>
     );
   }
